72473bb14a1a0a2edb1b37b3fbaf646da1e7f726
Nikita Koida <cheeeasy2501@gmail.com>
2022-05-17 00:38:42 +0300

    Added part of verification jwt

--- internal/book/model.go
+++ internal/book/model.go
@@ -3,7 +3,8 @@ package book
 import "github.com/google/uuid"
 
 type Book struct {
-	ID          uuid.UUID `json:"ID"`
-	Title       string    `json:"Title"`
-	Description string    `json:"Description"`
+	ID          uuid.UUID `json:"id"`
+	AuthorID    int64     `json:"authorId"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
 }
